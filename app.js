e3ca25b859b7ac925687097ee691b28265112730
Momansi96 <mansimohammed8@gmail.com>
2021-05-17 22:46:57 +0300

    finished lab12

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ let attempts = 0;
 let maxAttempts = 25;
 let attemptsEl = document.getElementById('attempts');
 let items = [];
+let itemsImagesNames = [];
+let itemsClicks = [];
+let itemsViews = [];
+
 
 function itemImg(itemName) {
-    //'cruisin-goat.jpg'.split('.') >>['cruisin-goat','jpg']
+    
     this.itemName = itemName.split('.')[0];
     this.source = 'img/' + itemName;
     this.clicks = 0;
     this.views = 0;
     items.push(this);
+    itemsImagesNames.push(this.itemName)
 }
 
 let itemImages = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'braekfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg', 'wine-glass.jpg']
@@ -43,6 +48,9 @@ function renderImg() {
     }
     while (centerImgIndex === leftImgIndex) {
         centerImgIndex = generateImage(); 
+    }
+    while (rightImgIndex === centerImgIndex) {
+        rightImgIndex = generateImage(); 
     }
 
     lImgEl.setAttribute('src', items[leftImgIndex].source);
@@ -84,10 +92,51 @@ function handelClicks(event) {
             liEl = document.createElement('li');
             ulEl.appendChild(liEl);
             liEl.textContent = `${items[i].itemName} has ${items[i].views} views and has ${items[i].clicks} clicks.`
+            itemsClicks.push(items[i].clicks);
+            itemsViews.push(items[i].views);
         }
         lImgEl.removeEventListener('click', handelClicks);
         rImgEl.removeEventListener('click', handelClicks);
         cImgEl.removeEventListener('click', handelClicks);
+        chartRender();
     }
 }
+function chartRender() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: itemsImagesNames,
+            datasets: [{
+                label: 'Num of Clicks',
+                data: itemsClicks,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                ],
+                borderWidth: 3
+            }, {
+                label: 'Num of Views',
+                data: itemsViews,
+                backgroundColor: [
+                    'rgba(75, 192, 192, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(75, 192, 192, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
 console.log(items)
